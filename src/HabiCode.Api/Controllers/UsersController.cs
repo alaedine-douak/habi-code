a7a22ee348ddef97c4026f84f3bd3d74b96840df
Alaedindouak <alaedin.hak@gmail.com>
2025-05-16 16:20:03 +0100

    Added GetCurrentUser method to retrieve authenticated user info

--- src/HabiCode.Api/Controllers/UsersController.cs
+++ src/HabiCode.Api/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
-﻿using HabiCode.Api.Database;
+﻿using System.Security.Claims;
+using HabiCode.Api.Database;
 using HabiCode.Api.DTOs.Users;
+using HabiCode.Api.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,7 +11,9 @@ namespace HabiCode.Api.Controllers;
 [Authorize]
 [ApiController] 
 [Route("users")]
-public sealed class UsersController(HabiCodeDbContext dbContext) : ControllerBase
+public sealed class UsersController(
+    HabiCodeDbContext dbContext) 
+    : ControllerBase
 {
     [HttpGet("{id}")]
     public async Task<ActionResult<UserDto>> GetUserById(string id)
@@ -17,6 +21,30 @@ public sealed class UsersController(HabiCodeDbContext dbContext) : ControllerBas
         UserDto? user = await dbContext
             .Users
             .Where(u => u.Id == id)
+            .Select(UserQueries.ProjectToDto())
+            .FirstOrDefaultAsync();
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+
+    [HttpGet("me")]
+    public async Task<ActionResult<UserDto>> GetCurrentUser()
+    {
+        string? identityUser = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (identityUser is null)
+        {
+            return Unauthorized();
+        }
+
+        UserDto? user = await dbContext
+            .Users
+            .Where(u =>  u.IdentityId ==  identityUser)
             .Select(UserQueries.ProjectToDto())
             .FirstOrDefaultAsync();
 
